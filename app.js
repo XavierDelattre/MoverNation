259ce61bde3c6cee432048d3b081dedfb11d24c7
Xavier Delattre <15686756+XavierDelattre@users.noreply.github.com>
2018-05-18 23:03:10 +0200

    Authentification made
    Secure but need to add some verification in form

--- app.js
+++ app.js
@@ -1,45 +1,112 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const db = require('sqlite3')
-const sequelize = require('sequelize')
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const	bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const User = require('./models/user');
 
-const app = express()
-const data = require('./dummy-data')
+const app = express();
 
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use("/assets", express.static(__dirname + '/assets'));
 app.engine('.hbs', expressHandlebars({
 defaultLayout: 'layout',
 extname: '.hbs'
-}))
-app.use("/assets", express.static(__dirname + '/assets'));
+}));
+app.use(session({
+    key: 'movernation_sid',
+    secret: '6dTE6Bsy86NdX4k5PzuZYAELDt64x4PnAb13AaNrAQdV5S2j7hgKLa228f8f636h',
+    resave: false,
+    proxy: true,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+app.use((req, res, next) => {
+    if (req.cookies.movernation_sid && !req.session.user) {
+        res.clearCookie('movernation_sid');        
+    }
+    next();
+});
+
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        console.log("User connected");
+    } else {
+        next();
+    }    
+};
 
 
-app.get('/', function(request, response){
-	response.render("index.hbs")
-})
+app.get('/', sessionChecker, (req, res) => {
+	res.render("index.hbs");
+});
 
-app.get('/about', function(request, response){
-	response.render("about.hbs")
-})
+app.get('/about', function(req, res){
+	res.render("about.hbs");
+});
 
-app.get('/contact', function(request, response){
-	response.render("contact.hbs")
-})
+app.get('/contact', function(req, res){
+	res.render("contact.hbs");
+});
 
-app.get('/login', function(request, response){
-	response.render("login.hbs")
-})
+app.route('/register')
+		.get(sessionChecker, (req, res) => {
+			res.render("register.hbs");
+		})
+		.post((req, res) => {
+			User.create({
+				username: req.body.username,
+				password: req.body.password,
+				email: req.body.email,
+				city: req.body.city,
+				birth: req.body.birth
+			})
+			.then(user => {
+				req.session.user = user.dataValues;
+				res.render("index.hbs");
+			})
+			.catch(err => {
+				console.log(err);
+				res.render("register.hbs");
+			});
+		});
 
-app.get('/register', function(request, response){
-	response.render("register.hbs")
-})
+app.route('/login')
+		.get(sessionChecker, (req, res) => {
+			res.render("login.hbs");
+		})
+		.post((req, res) => {
+			var username = req.body.username,
+					password = req.body.password;
 
-app.get('/accounts', function(request, response){
-	response.render(data.accounts)
-})
+			User.findOne({ where: { username: username} }).then(function (user){
+				if (!user){
+					console.log("No user found with this username.");
+					res.render("login.hbs");
+				} 
+				else if (!user.validPassword(password)) {
+					console.log("The password is incorrect.");
+					res.render("login.hbs");
+				} 
+				else {
+					req.session.user = user.dataValues;
+					res.render("index.hbs");
+				}
+			})
+		})
 
-app.get('/accounts/:id', function(request, response){
-	const id = request.params.id
-	response.render(data.accounts[id])
-})
+app.get('/accounts', function(req, res){
+	res.render(data.accounts)
+});
 
-app.listen(8080)
+app.get('/accounts/:id', function(req, res){
+	const id = req.params.id
+});
+
+app.listen(8080);
---
+++ models/user.js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt');
+
+var sequelize = new Sequelize('MoverNationDb', null, null, {
+	dialect: "sqlite",
+	storage: "./db/movernation.sqlite",
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+ var User = sequelize.define("users", {
+	username: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	email: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false,
+		validate: {
+			isEmail: true
+		}
+	},
+	city: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	birth: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+});
+User.prototype.validPassword = function(password){
+	return bcrypt.compareSync(password, this.password);
+};
+User.hook("beforeCreate", function(user){
+	user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(), null)
+});
+
+sequelize.sync({force: false})
+    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
+    .catch(err => console.log('This error occured', err));
+
+module.exports = User;
